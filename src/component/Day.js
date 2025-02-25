1953a913895582b1a9642eb71ef5095fcdaac038
aceofmath <aceofmath@naver.com>
2025-02-25 18:41:10 +0900

    json-server 버전 커밋

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,31 +1,22 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 import Word from "./Word";
 
 export default function Day() {
-    const { day } = useParams();
-    const [words, setWords] = useState([]);
+  const { day } = useParams();
+  const words = useFetch(`http://localhost:3001/words?day=${day}`);
 
-    useEffect(() => {
-        fetch(`http://localhost:3001/words?day=${day}`)
-            .then((res) => {
-                return res.json();
-            })
-            .then((data) => {
-                setWords(data);
-            });
-    }, [day]);
-
-    return (
-        <>
-            <h2>Day {day}</h2>
-            <table>
-                <tbody>
-                    {words.map((word) => (
-                        <Word word={word} key={word.id} />
-                    ))}
-                </tbody>
-            </table>
-        </>
-    );
+  return (
+    <>
+      <h2>Day {day}</h2>
+      {/* {words.length === 0 && <span>Loading...</span>} */}
+      <table>
+        <tbody>
+          {words.map((word) => (
+            <Word word={word} key={word.id} />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
